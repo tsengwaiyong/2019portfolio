d598656dd5c01d7adb76848d651b04f4318f7aec
Tseng <53459541+tsengwaiyong@users.noreply.github.com>
2020-06-01 13:48:40 +0800

    Add files via upload

--- jsp/index.js
+++ jsp/index.js
@@ -1,27 +1,12 @@
-function totop(){
-	$('body,html').animate({scrollTop:0},500);
-                return false;
+var moreoption = document.getElementById("moreoption");
+
+function showmoreoption(){
+	if(moreoption.style.height == "150px"){
+		moreoption.style.height = "0"
+	}
+	else{
+		moreoption.style.height = "150px";
+	}
+
+
 }
-
-
-function showmenu(){
-		
-        var tab = document.getElementById("tab");
-		
-		if(tab.className==='tab'){
-			$(".menu-sel").slideToggle();
-			tab.className='tab active';
-			
-		}
-		else {
-			$(".menu-sel").slideToggle();
-			tab.className='tab';
-		}
-	
-        
-}
-
-$(function () {
-	
-});
-
